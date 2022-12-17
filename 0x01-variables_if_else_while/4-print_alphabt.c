32edf248a47cadc5bbd0f91ea363b29e868f0969
Yabebal <yabebala@gmail.com>
2022-12-17 03:49:44 -0800

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - classify numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i, newL;
+
+	newL = '\n';
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'q' || i == 'e')
+			continue;
+		putchar(i);
+	}
+
+	putchar(newL);
+
+	return (0);
+}
