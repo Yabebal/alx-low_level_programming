1a32595c66b19fd34b1cf1618ab2c847e1719b0c
Yabebal <yabebala@gmail.com>
2022-12-17 03:57:41 -0800

    Number printing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - classify numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char newL, i;
+
+	newL = '\n';
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar(newL);
+
+	return (0);
+}
