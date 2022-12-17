81f51ee71a0a9ded61f18ed7b2dd98a39bd31c07
Yabebal <yabebala@gmail.com>
2022-12-17 11:44:59 -0800

    Hours print

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - print hours and minutes
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (l = 0; l <= 9; l++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + l);
+					_[utchar('\n');
+				}
+			}
+		}
+	}
+}
