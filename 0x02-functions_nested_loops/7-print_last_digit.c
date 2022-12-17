01cc5185e32f77c79e19bc8510384085ce9af9c6
Yabebal <yabebala@gmail.com>
2022-12-17 10:50:07 -0800

    lasr digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,16 @@
 #include "main.h"
-
 /**
- * print_last_digit - print last digit of n
+ * print_last_digit - last digit value of n
  * @n: integer
  * Return: Always 0
  */
 
 int print_last_digit(int n)
 {
+	if (n > 0)
+	{
+		return (n % 10);
+	}
 
-	return (n % 10);
+	return (-1 * n % 10);
 }
