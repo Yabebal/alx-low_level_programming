b0f77ba92244aa6b66cbcd2e4d3eec835a76f5c5
Yabebal <yabebala@gmail.com>
2022-12-18 10:27:56 -0800

    Is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,61 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks the letter is captital
+ * @c: is the letter
+ * Return: succuss
+ */
+
 int _isupper(int c)
 {
-  /* Checking the letter is upper or lower*/
-  switch (c)
-    {
-    case 'A':
-      return 1;
-    case 'B':
-      return 1;
-    case 'C':
-      return 1;
-    case 'D':
-      return 1;
-    case 'E':
-      return 1;
-    case 'F':
-      return 1;
-    case 'G':
-      return 1;
-    case 'H':
-      return 1;
-    case 'I':
-      return 1;
-    case 'J':
-      return 1;
-    case 'K':
-      return 1;
-    case 'L':
-      return 1;
-    case 'M':
-      return 1;
-    case 'N':
-      return 1;
-    case 'O':
-      return 1;
-    case 'P':
-      return 1;
-    case 'Q':
-      return 1;
-    case 'R':
-      return 1;
-    case 'S':
-      return 1;
-    case 'T':
-      return 1;
-    case 'U':
-      return 1;
-    case 'V':
-      return 1;
-    case 'W':
-      return 1;
-    case 'X':
-      return 1;
-    case 'Y':
-      return 1;
-    case 'Z':
-      return 1;
-    default:
-      return 0;
-    }
+	int i, count;
+
+	count = 0;
+	for (i = 'A'; i <= 'Z')
+	{
+		if (c == i)
+			count++;
+	}
+	if (count != 0)
+		return (1);
+	return (0);
 }
