397ba7ac2631cc57dc3cb0540a2be3d2659f8e1b
Yabebal <yabebala@gmail.com>
2022-12-18 19:22:20 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply two numbers
+ * @a: first integer
+ * @b: second argument
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c~
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: first integer
+ * @b: second argument
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
