96448d5b3efcdeb6f00423aa3e7c692f8da3c7da
Yabebal <yabebala@gmail.com>
2022-12-20 04:58:25 -0800

    printing numbers except 2 and 4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,8 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar('0' + i);
+		if (i != 2 || i != 4)
+			_putchar('0' + i);
 	}
 	_putchar('\n');
 }
