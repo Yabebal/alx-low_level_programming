d56f5bcc67141347150d3047357c167300c44622
Yabebal <yabebala@gmail.com>
2022-12-21 04:17:23 -0800

    Print numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,4 @@
 #include "main.h"
-
-/**
- * make_out - print large numbers
- * @n: input
- * Return: always 0
- */
-void make_out(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n == 0)
-		_putchar('0');
-	if ((n / 10) > 0)
-		make_out(n / 10);
-	_putchar(n % 10 + '0');
-}
-
 /**
  * more_numbers - print numbers from 0 to 14
  * Return: void
