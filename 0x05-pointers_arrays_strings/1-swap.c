7284f5434cc23286d4ab2859a7c25e38260aa562
Yabebal <yabebala@gmail.com>
2022-12-29 00:22:29 -0800

    1. Dont swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - resetpointer to 98
+ * @a: first value
+ * @b: second value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
